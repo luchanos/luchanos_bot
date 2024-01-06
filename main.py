eece72be68bf2f5aeeb7370d144c7045ee0b1705
luchanos <lukash_91@mail.ru>
2024-01-06 17:58:24 +0100

    init

---
+++ main.py
@@ -0,0 +1,102 @@
+import telebot
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from envparse import Env
+from telebot import types
+
+
+env = Env()
+TOKEN = env.str("TOKEN")
+
+scope = ["https://spreadsheets.google.com/feeds",
+         'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file",
+         "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name('key.json', scope)
+client = gspread.authorize(creds)
+sheet = client.open('test').sheet1  # Open the sheet
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=["start", ])
+def start(message):
+    markup = types.InlineKeyboardMarkup(row_width=1)
+
+    btn1 = types.InlineKeyboardButton("FAQ (частые вопросы)", callback_data="faq")
+    btn2 = types.InlineKeyboardButton("Программа воркшопа", callback_data="program")
+    btn3 = types.InlineKeyboardButton("Оставить заявку на воркшоп", callback_data="apply")
+
+    markup.add(btn1, btn2, btn3)
+    bot.reply_to(message,
+                 "Желаете записаться на воркшоп по ООП или получить по нему информацию?"
+                 "Тогда жмите соответствующую кнопку!",
+                 reply_markup=markup)
+
+
+WORKSHOP_CONTENT = """
+**1️⃣ Введение в ООП**
+  - Определение и основные концепции ООП.
+  - Исторический контекст и преимущества ООП.
+
+**2️⃣ Основные принципы ООП**
+  - Инкапсуляция: скрытие данных и поведения.
+  - Наследование: повторное использование и расширение кода.
+  - Полиморфизм: использование общих интерфейсов для разных типов.
+  - Абстракция: упрощение сложных реальностей.
+
+**3️⃣ Классы и Объекты**
+  - Определение и создание классов.
+  - Инициализация объектов (конструкторы).
+  - Атрибуты (свойства) и методы (функции).
+
+**4️⃣ Углубление в Классы и Объекты**
+  - Статические методы и переменные класса.
+  - Приватные и защищенные атрибуты и методы.
+  - "Magic methods".
+
+**5️⃣ Наследование в Деталях**
+  - Создание подклассов.
+  - Переопределение и расширение методов.
+  - Множественное наследование и его особенности.
+
+**6️⃣ Полиморфизм и Абстракция**
+  - Примеры полиморфизма в Python.
+  - Абстрактные базовые классы.
+"""
+
+FAQ = """
+Q: Когда начнется воркшоп?
+A: Занятия будут проходить с 22 января по понедельникам в 19 часов Мск
+
+Q: Будет ли вестись запись занятий?
+A: Да, все записи будут выгружаться в телеграм сразу после занятия и будут доступны в чате в течение года
+
+Q: Будет ли домашка?
+A: Да, будут предоставлены рекомендуемые к выполнению задания, а также будет проведено разборное занятие
+"""
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    user_id = call.from_user.id
+    username = call.from_user.username or "No Username"  # Some users might not have a username
+
+    if call.data == "faq":
+        bot.answer_callback_query(callback_query_id=call.id)
+        bot.send_message(call.message.chat.id, FAQ, parse_mode="Markdown")
+        sheet.append_row([user_id, username, "FAQ"])
+
+    elif call.data == "program":
+        bot.answer_callback_query(callback_query_id=call.id)
+        bot.send_message(call.message.chat.id, WORKSHOP_CONTENT, parse_mode='Markdown')
+        sheet.append_row([user_id, username, "Program"])
+
+    elif call.data == "apply":
+        bot.answer_callback_query(callback_query_id=call.id)
+        bot.send_message(call.message.chat.id, "Ваша заявка оставлена! Большое спасибо!"
+                                               "Я скоро вернусь со ссылкой на оплату.")
+        sheet.append_row([user_id, username, "Apply"])
+
+
+bot.polling()
